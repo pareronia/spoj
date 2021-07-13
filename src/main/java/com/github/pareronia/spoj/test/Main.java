e40e2f9787f19f36d167d8c10d16ebfb9855bbbc
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-13 16:59:43 +0200

    TEST

---
+++ src/main/java/com/github/pareronia/spoj/test/Main.java
@@ -0,0 +1,111 @@
+package com.github.pareronia.spoj.test;
+
+import static java.util.Arrays.asList;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Supplier;
+
+public class Main {
+    
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Main(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> this.out.println(r.value));
+    }
+    
+    public void solve() {
+        try (final Scanner sc = new Scanner(new InputStreamReader(this.in))) {
+            final List<Result<?>> results = new ArrayList<>();
+            int next = sc.nextInt();
+            while (next != 42) {
+                results.add(new Result<>(next));
+                next = sc.nextInt();
+            }
+            output(results);
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Main.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Main(sample, is, out).solve();
+    	
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Main.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        return "sample".equals(System.getProperty("spoj"));
+    }
+    
+    private static final class Result<T> {
+        private final T value;
+
+        public Result(final T value) {
+            this.value = value;
+        }
+    }
+}
